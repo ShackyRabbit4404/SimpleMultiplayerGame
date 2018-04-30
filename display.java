cb08af4ecb237243013dcdd63108ef634aa64613
Brian Hulbert <Brian.hulbert@me.com>
2018-04-30 18:10:41 -0400

    Initial sharing of project

---
+++ display.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+import javax.swing.*;
+public class display extends JPanel{
+    public display(){
+        super();
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+    }
+}
---
+++ main.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class main{
+    public static void main(String[] args){
+        JFrame frame = new JFrame();
+        display screen = new display();
+        frame.add(screen);
+        
+    }
+}
