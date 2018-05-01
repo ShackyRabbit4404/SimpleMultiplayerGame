c07173e698d0292965525ec44f202feae0d1a149
Tristan <tblonswa@syr.edu>
2018-05-01 14:30:04 -0400

    Server progress

--- Client.java
+++ Client.java
@@ -1,10 +1,24 @@
 import java.util.*;
+import java.net.*;
+import java.io.*;
 public class Client{
+    Socket sock;
+    PrintStream ps;
+    BufferedReader din;
+    InputStream is;
+    
     public Client(){
-        
+        try {
+            sock = new Socket("127.0.0.1", 11211);
+            ps = new PrintStream(sock.getOutputStream());
+            din = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            is = sock.getInputStream();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     public void changeX(int a,String name){
-        
+        ps.println("c:x:");
     }
     public void changeY(int a,String name){
         
