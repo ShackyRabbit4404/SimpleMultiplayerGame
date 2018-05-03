9218b68a198c1b332741045e45ffb41845d7913c
Brian Hulbert <Brian.hulbert@me.com>
2018-05-02 21:18:56 -0400

    collisoion fix?

--- display.java
+++ display.java
@@ -36,17 +36,17 @@ public class display extends JPanel{
     public void getAndSetInfo(){
         int x = new Client().getX(name);
         int y = new Client().getY(name);
-        if(W == true && y > 0 && isTouching(0,-10) == false)
+        if(W == true && y > 0 && isTouching() == false)
              new Client().changeY(-5,name);
-        if(A == true && x > 0 && isTouching(-10,0) == false)
+        if(A == true && x > 0 && isTouching() == false)
              new Client().changeX(-5,name);
-        if(S == true && y < 1080 && isTouching(10,0) == false)
+        if(S == true && y < 1080 && isTouching() == false)
              new Client().changeY(5,name);
-        if(D == true && x < 1910 && isTouching(0,10) == false)
+        if(D == true && x < 1910 && isTouching() == false)
              new Client().changeX(5,name);    
     }
-    public boolean isTouching(int x,int y){
-        Rectangle r1 = new Rectangle(new Client().getX(name) + x,new Client().getY(name) + y,20,20);
+    public boolean isTouching(){
+        Rectangle r1 = new Rectangle(new Client().getX(name) + 5,new Client().getY(name) + 5,30,30);
         for(Player p: players){
             if(!p.getName().equals(name)){
                 Rectangle r2 = new Rectangle(p.getX(),p.getY(),20,20);
